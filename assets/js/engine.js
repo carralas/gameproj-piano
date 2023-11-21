687f9577533292e5f8e7e53a079b561cfaa03880
lucas! <carralas@icloud.com>
2023-11-21 17:07:35 -0300

    feat: piano keys are function and play a note when pressed

---
+++ assets/js/engine.js
@@ -0,0 +1,33 @@
+const state = {
+    /* views - variáveis para exibição na tela */
+    view: {
+        keys: document.querySelectorAll('.key')
+    },
+    /* values - variáveis para controle interno */
+    values: {
+    },
+    /* actions - variáveis que cotrolam ações na engine */
+    actions: {
+    },
+    /* objects - variáveis que carregam informações externas */
+    objects: {
+        audio: new Audio()
+    }
+}
+
+function addListenerKey() {
+    state.view.keys.forEach((key) => {
+        key.addEventListener('click', () => playTune(key.dataset.key))
+    })
+}
+
+function playTune(note) {
+    state.objects.audio.src = `./assets/audio/${note.toLowerCase()}.wav`
+    state.objects.audio.play()
+}
+
+function main() {
+    addListenerKey()
+}
+
+main();
